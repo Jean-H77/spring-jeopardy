856efe7b73fca3bca7f0480dfe2c2df6196d69a2
Jean <55899462+Jean-H77@users.noreply.github.com>
2024-01-13 15:36:43 -0800

    Move Optional logic to Question service

--- src/main/java/org/springjeopardy/controller/QuestionController.java
+++ src/main/java/org/springjeopardy/controller/QuestionController.java
@@ -2,21 +2,15 @@ package org.springjeopardy.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.RestController;
 import org.springjeopardy.model.QuestionModel;
 import org.springjeopardy.service.QuestionService;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
-
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
-@RequestMapping("api/question")
+@RequestMapping("api/v1/question")
 @RequiredArgsConstructor
 @Slf4j
 public class QuestionController {
@@ -30,19 +24,11 @@ public class QuestionController {
 
     @GetMapping("/random/{round}")
     public QuestionModel getRandomQuestion(@PathVariable String round) {
-        Optional<QuestionModel> questionModelOptional = questionService.getRandomQuestion(round);
-        if(questionModelOptional.isPresent()) {
-            return questionModelOptional.get();
-        }
-        throw new ResponseStatusException(NOT_FOUND, "Unable to find round of " + round);
+        return questionService.getRandomQuestion(round);
     }
 
     @GetMapping("/{question}")
     public QuestionModel findByQuestion(@PathVariable String question) {
-        Optional<QuestionModel> questionModelOptional = questionService.getByQuestion(question);
-        if(questionModelOptional.isPresent()) {
-            return questionModelOptional.get();
-        }
-        throw new ResponseStatusException(NOT_FOUND, "Unable to find jeopardy data of the question" + question);
+        return questionService.getByQuestion(question);
     }
 }
--- src/main/java/org/springjeopardy/service/QuestionService.java
+++ src/main/java/org/springjeopardy/service/QuestionService.java
@@ -2,11 +2,14 @@ package org.springjeopardy.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import org.springjeopardy.entity.QuestionEntity;
 import org.springjeopardy.model.QuestionModel;
 import org.springjeopardy.repository.QuestionRepository;
 
 import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -19,14 +22,22 @@ public class QuestionService {
         return toModel(questionEntity);
     }
 
-    public Optional<QuestionModel> getRandomQuestion(String round) {
-        Optional<QuestionEntity> questionEntity = Optional.ofNullable(questionRepository.getRandomQuestion(round));
-        return questionEntity.map(QuestionService::toModel);
+    public QuestionModel getRandomQuestion(String round) {
+        Optional<QuestionEntity> optionalQuestionEntity = Optional.ofNullable(questionRepository.getRandomQuestion(round));
+        if(optionalQuestionEntity.isPresent()) {
+            QuestionEntity entity = optionalQuestionEntity.get();
+            return toModel(entity);
+        }
+        throw new ResponseStatusException(NOT_FOUND, "Unable to find round of " + round);
     }
 
-    public Optional<QuestionModel> getByQuestion(String question) {
-        Optional<QuestionEntity> questionEntity = Optional.ofNullable(questionRepository.findByQuestion(question));
-        return questionEntity.map(QuestionService::toModel);
+    public QuestionModel getByQuestion(String question) {
+        Optional<QuestionEntity> optionalQuestionEntity = Optional.ofNullable(questionRepository.findByQuestion(question));
+        if(optionalQuestionEntity.isPresent()) {
+            QuestionEntity entity = optionalQuestionEntity.get();
+            return toModel(entity);
+        }
+        throw new ResponseStatusException(NOT_FOUND, "Unable to find jeopardy data of the question" + question);
     }
 
     private static QuestionModel toModel(QuestionEntity questionEntity) {
